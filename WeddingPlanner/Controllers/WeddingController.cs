fa8ff919e85dfa358b792d37d9f315947636eca5
SimplyyJoji <59666562+SimplyyJoji@users.noreply.github.com>
2021-08-17 17:46:09 -0700

    Created dashboard

--- WeddingPlanner/Controllers/WeddingController.cs
+++ WeddingPlanner/Controllers/WeddingController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WeddingPlanner.Models;
@@ -18,6 +20,32 @@ namespace WeddingPlanner.Controllers
             return View("New");
         }
 
+        [HttpPost("/weddings/create")]
+        public IActionResult Create(Wedding newWedding)
+        {
+            // Every time a form is submitted, check the validations.
+            if (ModelState.IsValid == false)
+            {
+                // Go back to the form so error messages are displayed.
+                return View("New");
+            }
+
+            // If any above custom errors were added, ModelState would now be invalid.
+        
+            db.Weddings.Add(newWedding);
+            db.SaveChanges();
+
+            HttpContext.Session.SetInt32("WeddingId", newWedding.WeddingId);
+            HttpContext.Session.SetString("NameOne", newWedding.NameOne);
+            return RedirectToAction("Dashboard");
+        }
+
+        [HttpGet("/dashboard")]
+        public IActionResult Dashboard()
+        {
+            List<Wedding> allWeddings = db.Weddings.ToList();
+            return View("Dashboard", allWeddings);
+        }
 
     }
 }
